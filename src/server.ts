026756d9454901f3ba5ab18d0d7e7035a4d08c7b
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-03-13 23:33:51 -0300

    routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
+import { getAttendeeBadge } from './routes/get-attendee-badge'
+import { getEvent } from './routes/get-event'
+import { registerForEvent } from './routes/register-for-event'
 
 const app = fastify()
 
@@ -11,6 +14,9 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
